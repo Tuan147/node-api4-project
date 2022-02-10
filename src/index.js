d24b73c3c65ac6bb0685ffe4701c8f2c143ff9ee
Tuan147 <90153951+Tuan147@users.noreply.github.com>
2022-02-10 14:29:30 -0500

    added in new port

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const server = require('./server')
+const server = express();
 
 require('dotenv').config();
 
@@ -12,4 +12,49 @@ server.listen(PORT, () => {
 
 server.use('*', (req, res) => {
     res.send('<h1>Welcome to Retroville</h1>')
-})
+});
+
+const users = [
+    {
+        username: 'Tuan',
+        password: 'badpass'
+    },
+    {
+        username: 'Jimmy',
+        password: 'goodpass'
+    },
+];
+
+const register = [
+    {
+        username: 'Carl',
+        password: 'jimmysmom'
+    }
+];
+
+const login = [
+    {
+        username: 'Sheen',
+        password: 'ultralord'
+    }
+];
+
+server.get('/api/users', (req, res) => {
+    res.status(200).json(users)
+});
+
+server.post('/api/register', (req, res) => {
+    res.status(201).json(register)
+});
+
+server.post('/api/login', (req, res) => {
+    if(!login) {
+        res.status(404).json({
+            message: 'Username and password incorrect'
+        })
+    } else {
+        res.status(200).json({
+            message: 'Welcome to RetroVille'
+        })
+    }
+});
